fb308f7fc2eeab730670a826f2e2dd50b3f5a25b
oeuillot <olivier.oeuillot@gmail.com>
2013-12-06 15:04:13 +0100

    + Bug fixes

--- sample.js
+++ sample.js
@@ -4,7 +4,9 @@
 
 var Xpl=require("./lib/xpl-api");
 
-var xpl=new Xpl();
+var xpl=new Xpl({
+	source: "nodejs-sample"
+});
 
 xpl.on("message", function(event, address) {
 	console.log("Receive event ", event, " from ", address);
@@ -13,4 +15,10 @@ xpl.on("message", function(event, address) {
 
 xpl.wait(function(error) {
 	console.log("Wait return ", error);
+});
+
+xpl.sendXplTrig({
+	device: "cul",
+	type: "temp",
+	current: 20.4
 });
